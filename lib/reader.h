7fb4043a042690611579ace39f6d4e94fb1bbab1
UnixSD <zaharkovtonuk7@gmail.com>
2025-11-14 08:54:58 +0200

    Update license text and function signatures in reader.h

--- lib/reader.h
+++ lib/reader.h
@@ -7,7 +7,7 @@
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   Unix Engine is distributed in the hope that it will be useful,
+   UASM is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
@@ -15,5 +15,14 @@
    You should have received a copy of the GNU General Public License
    along with UASM.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #pragma once 
+#include "preproc.h"
+#include "linker.h"
+
+#if _cplusplues < 202009L
+void readINCL(char file) const;
+void readASM(char file) const;
+#endif
+
+void readINCL(ifstream file) const;
+void readASM(ifstream file) const;
