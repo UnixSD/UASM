ace5f2fd98022d4780271c9ee66abcb3443cf741
UnixSD <zaharkovtonuk7@gmail.com>
2025-11-18 14:19:28 +0200

    Add linker.h header file for object linking

---
+++ lib/linker.h
@@ -0,0 +1,37 @@
+/*  Copyright (C) 2025 UnixSD
+
+   This file is part of UASM.
+
+   Unix Engine is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Unix Engine is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with UASM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "preproc.h"
+#if __cplusplus < 202002L
+#   include <filesystem>
+#   define __CPP17__
+namespace fs = std::filesystem
+bool linkObjects(vector<string> files,
+                 char &output);
+#endif
+
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+using namespace std;
+/* removed 'export' */ bool linkObjects(vector<string> files);
+
+void trasferResults(string objFile);
